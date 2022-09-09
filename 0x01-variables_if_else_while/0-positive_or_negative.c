07478d2b659bf78602a224a0ec7e026dbbe589e3
Goodness-mma <goodnessmichel@gmail.com>
2022-09-09 13:54:23 -0700

    Stubborn

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,38 +1,22 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
 /**
- * main - responsible for starting the execution and termination of the program
- * Return - aways 0 (Success)
- */ 
-
+ * main - this is responsible for starting the execution and termination of 0
+ * Return: 0
+ */
 int main(void)
-
 {
-
 	int n;
 
-
-
 	srand(time(0));
-
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
-
 		printf("%d is positive\n", n);
-
 	else if (n == 0)
-
 		printf("%d is zero\n", n);
-
 	else if (n < 0)
-
 		printf("%d is negative\n", n);
-
 	return (0);
-
 }
